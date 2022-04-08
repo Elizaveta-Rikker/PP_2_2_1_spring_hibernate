279446cc6590b78426966108f5db43f452a22525
Liza Rikker <lrikker@yandex.ru>
2022-04-08 20:19:14 +0300

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,17 +16,17 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Model1", 1)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("Model2", 2)));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
+      List<User> users2 = userService.listUsers();
+      for (User user : users2) {
          System.out.println("Id = "+user.getId());
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println("Car Model = "+user.getCar().getModel());
+         System.out.println("Car Series = "+user.getCar().getSeries());
          System.out.println();
       }
 
